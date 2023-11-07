c4a9784b91806a40f8002f635818e59133cbe907
minyou73 <145949811+minyou73@users.noreply.github.com>
2023-11-07 14:36:23 +0900

    article모델링&마이그레이션

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from accounts.models import User
+from django.conf import settings
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    # 유저 모델을 참조하는 경우
+    # 방법1. (권장하지 않음)
+    # user = models.ForeignKey(, on_delete=models.CASCADE)
+
+    # 방법2. (권장) settings.AUTH_USER_MODEL=='accounts.User'
+    # user = models.ForeeignKey(seetings.AUTH_USER_MODEL, on_delete=models.CASCADE )
+
+    # 방법3. (권장)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
